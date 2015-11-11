3b45d8ae18475bc19f69635f37c73c085dbd3e11
Patrick Laermann <patrick@linux-4xun.site>
2015-11-11 14:37:08 +0100

    Änderung

--- statistics.cxx
+++ statistics.cxx
@@ -3,16 +3,36 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+void f_rand(double*x,const int N){
+  for(int i=0; i<N; i++){
+	x[i] = rand() / double (RAND_MAX); 
+        //cout  << x[i] << endl;
+  }
+  
+}
+
+void f_stat(double*x, double& mean, double& var,const int N){
+      mean=0;
+      var=0;
+      for(int i=0; i<N; i++){
+	mean =  mean + x[i];
+	
+      }
+        mean = mean / N ; 
+      for(int i=0; i<N; i++){
+	var = var +  pow((x[i] - mean),2);
+	
+      }
+        var = var / N ;
+}
 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
-
-   // Some lines here....
+   
+   f_rand(p,N);
+   f_stat(p,mean,var,N);
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
